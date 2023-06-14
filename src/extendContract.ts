49194b7958398aa2a3fb5ed801d47665c4a7d324
YannBcf <Yannbeats.pro@gmail.com>
2023-06-15 01:02:41 +0200

    feat: implement contract extending mechanism

---
+++ src/extendContract.ts
@@ -0,0 +1,19 @@
+import type { RpcContract } from "./types.ts";
+
+type MergeContracts<Contract1 extends RpcContract, Contract2 extends RpcContract> = {
+    [K in keyof Contract1]: K extends keyof Contract2 ? Contract2[K] : Contract1[K];
+} & Omit<Contract2, keyof Contract1>;
+
+/**
+ * This method allow you to take a contract as input and override/extend it
+ *
+ * @param contract1
+ * @param contract2
+ * @returns
+ */
+export function extend<
+    const Contract1 extends RpcContract,
+    const Contract2 extends RpcContract
+>(contract1: Contract1, contract2: Contract2) {
+    return { ...contract1, ...contract2 } as MergeContracts<Contract1, Contract2>;
+}
