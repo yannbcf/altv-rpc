116d4768020bdcaf5e56f615672e452f8e06138c
YannBcf <Yannbeats.pro@gmail.com>
2023-06-17 14:54:53 +0200

    build: improve build steps

--- build.js
+++ build.js
@@ -5,6 +5,25 @@ import { build } from "esbuild";
 const BASE = process.cwd();
 const ENTRY_POINT = join(BASE, "src/index.ts");
 
+function _build(inPath, outPath, external) {
+    build({
+        bundle: true,
+        minify: true,
+        format: "esm",
+        platform: "node",
+        target: "esnext",
+        keepNames: true,
+        entryPoints: [
+            {
+                in: inPath,
+                out: outPath,
+            },
+        ],
+        outdir: "dist",
+        external: external,
+    });
+}
+
 try {
     const t = performance.now();
 
@@ -13,70 +32,10 @@ try {
     // });
 
     await Promise.all([
-        build({
-            bundle: true,
-            minify: true,
-            format: "esm",
-            platform: "node",
-            target: "esnext",
-            keepNames: true,
-            entryPoints: [
-                {
-                    in: "src/index.ts",
-                    out: "index",
-                },
-            ],
-            outdir: "dist",
-            external: ["zod"],
-        }),
-        build({
-            bundle: true,
-            minify: true,
-            format: "esm",
-            platform: "node",
-            target: "esnext",
-            keepNames: true,
-            entryPoints: [
-                {
-                    in: "src/$client.ts",
-                    out: "$client",
-                },
-            ],
-            outdir: "dist",
-            external: ["zod", "alt-client"],
-        }),
-        build({
-            bundle: true,
-            minify: true,
-            format: "esm",
-            platform: "node",
-            target: "esnext",
-            keepNames: true,
-            entryPoints: [
-                {
-                    in: "src/$server.ts",
-                    out: "$server",
-                },
-            ],
-            outdir: "dist",
-            external: ["zod", "alt-server"],
-        }),
-        build({
-            bundle: true,
-            minify: true,
-            format: "esm",
-            platform: "node",
-            target: "esnext",
-            keepNames: true,
-            entryPoints: [
-                {
-                    in: "src/$typeOnly.ts",
-                    out: "$typeOnly",
-                },
-            ],
-            outdir: "dist",
-            external: ["zod"],
-        }),
+        _build("src/index.ts", "index", ["zod"]),
+        _build("src/$types/$client.ts", "$client", ["zod", "alt-client"]),
+        _build("src/$types/$server.ts", "$server", ["zod", "alt-server"]),
+        _build("src/$types/$typeOnly.ts", "$typeOnly", ["zod"])
     ]);
 
     console.log(`\n> Build finished ! (done in ${(performance.now() - t).toFixed(3)} ms)\n`);
